5bad52afc717b76d1f64265e9bb2fd1a84bcc166
Rafael Serna <rserna@pucp.edu.pe>
2023-01-19 20:25:10 -0500

    Objetos iniciales

---
+++ Detalle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Detalle
+{
+    public int Id { get; set; }
+    public string? NombreProducto { get; set; }
+    public int Cantidad { get; set; }
+    public decimal Monto { get; set; }
+
+}
---
+++ Venta.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Venta
+{
+    public int IdVenta { get; set; }
+    public string Cliente { get; set; }
+    public string RUCCliente { get; set; }
+    public List<Detalle> Detalles { get; set; }
+}
