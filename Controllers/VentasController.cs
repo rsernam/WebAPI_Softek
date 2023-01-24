0c04009098a761425bb94932f0a4d7b874bcabe3
Rafael Serna <rserna@pucp.edu.pe>
2023-01-24 11:13:21 -0500

    Seguridad, operaciones POST

--- Controllers/VentasController.cs
+++ Controllers/VentasController.cs
@@ -1,23 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebAPI_Softek.Models;
+using WebAPI_Softek.Attributes;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebAPI_Softek.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [ApiKey]
     public class VentasController : ControllerBase
     {
 
-            //readonly IVentaRepository _ventaRepository;
         private readonly VentasContext _context;
-        /*
-        public VentasController(IVentaRepository ventaRepository)
-            {
-                _ventaRepository = ventaRepository;
-            }
-        */
 
         public VentasController(VentasContext context)
         {
@@ -28,22 +23,6 @@ namespace WebAPI_Softek.Controllers
 
 
         }
-
-
-
-        /*
-           [HttpGet]
-         public ActionResult<List<Venta>> Get()
-            {
-                return Ok(_ventaRepository.ObtenerVentas());
-            }
-        /*
-                [HttpGet("{id}")]
-                public ActionResult<Venta> ObtenerVenta(int id)
-                {
-                    return Ok(_ventaRepository.ObtenerVentasPorId(id));
-                }
-        */
 
 
 
